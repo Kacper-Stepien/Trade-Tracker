e97fca16d86b04c0c3d7f8e2b302f001ecf263d8
Kacper Stępień <kacper2007x48@gmail.com>
2026-02-12 18:46:05 +0100

    Add role to JWT payload to fix AdminGuard permission check

--- api/src/auth2/strategies/jwt.strategy.ts
+++ api/src/auth2/strategies/jwt.strategy.ts
@@ -18,7 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: { sub: number; email: string }) {
-    return { sub: payload.sub, email: payload.email };
+  async validate(payload: { sub: number; email: string; role: string }) {
+    return { sub: payload.sub, email: payload.email, role: payload.role };
   }
 }
